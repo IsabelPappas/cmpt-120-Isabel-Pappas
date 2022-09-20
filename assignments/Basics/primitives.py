9c7b35ec2e180ce3c1f6fc236a61635513e2a8ca
Izzy Pappas <izzy@148-100-187-171.foxnet.marist.edu>
2022-09-19 20:33:54 -0400

    adding all of my completed labs

---
+++ assignments/Basics/primitives.py
@@ -0,0 +1,26 @@
+def get_int():
+    '''
+    Returns a variable of type 'int'.
+    '''
+    return 4
+
+
+def get_float():
+    '''
+    Returns a variable of type 'float'.
+    '''
+    return 0.14
+
+
+def get_str():
+    '''
+    Returns a variable of type 'str'.
+    '''
+    return "hello"
+
+
+def get_bool():
+    '''
+    Returns a variable of type 'bool'.
+    '''
+    return True
